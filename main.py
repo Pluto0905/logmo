065d4fb84bf3eb800e6efa10cf4b15a7ab974e7d
sogo <plutochan09051930@gmail.com>
2026-03-06 14:27:00 +0000

    websocketを実装

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 import shutil
 
 #webアプリの土台。このURLにアクセスされたらこの処理をする、というルールを追加していく。
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
 #静的ファイルを配信する
 from fastapi.staticfiles import StaticFiles
 
 #ファイルをブラウザに返す
 from fastapi.responses import FileResponse
+
+import asyncio
+
+
+def fetch_storage():
+    usage = shutil.disk_usage("/")
+    return {
+        "total_gb": round(usage.total / (1024 ** 3), 1),
+        "used_gb": round(usage.used / (1024 ** 3), 1),
+        "free_gb": round(usage.free / (1024 ** 3), 1),
+    }
 
 
 #webアプリの司令塔、インスタンスを作成する
@@ -18,7 +29,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-#関数をデコレートしてwebアプリの一部としてのインターフェースとなっている
+#関数をデコレートしてwebアプリの一部としてのインターフェース(/がAPI)となっている
 @app.get("/")
 def read_root():
     return FileResponse("static/index.html")
@@ -26,13 +37,11 @@ def read_root():
 
 @app.get("/storage")
 def get_storage():
-    usage = shutil.disk_usage("/")
+    return fetch_storage()
 
-    total_gb = usage.total / (1024 ** 3)
-    used_gb = usage.used / (1024 ** 3)
-    free_gb = usage.free / (1024 ** 3)
-    return {
-        "total_gb": round(total_gb, 1),
-        "used_gb": round(used_gb, 1),
-        "free_gb": round(free_gb, 1),
-    }
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket): #Websocket型のデータがwebsocketという変数にFastAPIが自動で代入する。WebSocketとはhttpのような通信方式の名前
+    await websocket.accept()
+    while True:
+        await websocket.send_json(fetch_storage())
+        await asyncio.sleep(5)
